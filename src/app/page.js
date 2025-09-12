e827aed57531a98279b493eb0a5ca32017176723
Maria Cecília <o.cecilia@aluno.ifsp.edu.br>
2025-09-12 17:55:30 -0300

    Section Sobre nós concluída

--- src/app/page.js
+++ src/app/page.js
@@ -7,6 +7,7 @@ import Cash from '../../public/icons/cash-outline.svg';
 import Construct from '../../public/icons/construct.svg';
 import SetaD from '../../public/icons/chevron-forward.svg';
 import SetaE from '../../public/icons/chevron-back-outline.svg';
+import Gallery from '../../public/img/imageGroup.png'
 
 export default function Home() {
   return (
@@ -65,8 +66,32 @@ export default function Home() {
         </div>
       </section>
 
+      {/*Sobre nós*/}
       <section className={styles.about}>
+        <div className={styles.container_about}>
+          <div className={styles.container_text_about}>
+            <div className={styles.container_title_about}>
+              <h1>Sobre a</h1>
+              <h1 className={styles.title_about}>Villa Indaiá</h1>
+            </div>
 
+            <p>
+              Na Villa Indaiá, acreditamos que cada imóvel conta uma história — e estamos aqui para ajudar você a escrever a sua.
+              Localizada no coração da nossa comunidade, somos especializados em venda de imóveis residenciais e comerciais,
+              sempre com foco em oferecer segurança, transparência e atendimento personalizado.<br /><br />
+
+              Nosso compromisso vai além da negociação: buscamos compreender as necessidades e sonhos de cada cliente para
+              encontrar o imóvel ideal, aquele que combina conforto, localização e valor justo. Trabalhamos com uma equipe
+              experiente, apaixonada pelo que faz, e sempre atualizada com as tendências do mercado imobiliário.<br /><br />
+
+              Na Villa Indaiá, você encontra mais do que imóveis: encontra parceiros de confiança para realizar seu projeto de vida.
+            </p>
+          </div>
+
+          <Image src={Gallery} alt="conjunto de imagens" className={styles.image}/>
+        </div>
+
+        <h2>Villa Indaiá – O lugar certo para o seu próximo capítulo.</h2>
       </section>
     </div>
   );
